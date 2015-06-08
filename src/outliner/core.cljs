717658f73c5734fb59b1695d5b313c90cbd137b8
minikomi <nerdfunk@gmail.com>
2015-06-08 19:40:28 +0900

    list is sortable somewhat

--- src/outliner/core.cljs
+++ src/outliner/core.cljs
@@ -15,6 +15,8 @@
 
 (enable-console-print!)
 
+(.initializeTouchEvents js/React true)
+
 ;;; state
 ;;; ------------------------------------------------------------------------------
 
@@ -22,7 +24,10 @@
   {:mode     :tree
    :drag-idx nil
    :over-idx false
+   :window-width 0
    :entries ["Red" "Green" "Blue" "Yellow" "Black" "White" "Orange"]
+   :bounding-boxes []
+   :hovered nil
    })
 
 ;;; handlers
@@ -35,85 +40,123 @@
    (merge db initial-state)))
 
 (register-handler                
- :drag-start
+ :window-width
  (fn 
-   [db [_ n]]
-   (assoc db :drag-idx n)))
+   [db [_ w]]
+   (assoc db :window-width w)))
 
 (register-handler                
- :drag-over
+ :bounding-boxes
  (fn 
-   [db [_ over-idx]]
-   (case over-idx
-     "placeholder" db 
-     "dummy-element" (assoc db :over-idx :end)
-     (assoc db :over-idx (js/parseInt over-idx 10)))))
+   [db [_ boxes]]
+   (assoc db :bounding-boxes boxes)))
+
+(defn find-first [f coll]
+  (first (drop-while (complement f) coll)))
 
 (register-handler                
- :drag-end
+ :touchmove
  (fn 
-   [{:keys [over-idx drag-idx] :as db} [_ n]]
-   (-> db
-       (update-in [:entries]
-                  (fn [entries]
-                    (let [dragged (entries drag-idx)
-                          disj-entries (into
-                                        (subvec entries 0 drag-idx)
-                                        (subvec entries (inc drag-idx)))]
-                      (if (= :end over-idx)
-                        (conj disj-entries dragged)
-                        (into
-                         (conj (subvec disj-entries 0 over-idx) dragged)
-                         (subvec disj-entries over-idx))))))
-       (assoc :over-idx false :drag-idx false)
-       )))
+   [db [_ [x y]]]
+   (if (nil? (:activated db))
+     db
+     (let [hover-idx
+           (get (find-first (fn [[box-top box-bottom n]]
+                              (and (<= box-top y)
+                                   (> box-bottom y)
+                                   ))
+                            (:bounding-boxes db)) 2)]
+       (if (nil? hover-idx)
+         db
+         (assoc db :hovered hover-idx))
+       ))))
+
+(register-handler
+ :activate
+ (fn [db [_ n id]]
+   (if (nil? (:activated db))
+     (assoc db :activated {:idx n :touchid id})
+     db)))
+
+(defn vec-swap [v idx1 idx2]
+  (assoc v idx2 (v idx1) idx1 (v idx2)))
+
+(register-handler
+ :touchend
+ (fn [db [_ id]]
+   (if (or (nil? (:activated db)) (not= id (:touchid (:activated db))))
+     db
+     (let [entries (:entries db)
+           hovered (:hovered db)
+           new-entries (if (nil? hovered)
+                         entries
+                         (vec-swap entries
+                                   (js/parseInt hovered 10)
+                                   (js/parseInt (:idx (:activated db)) 10)))]
+
+       (assoc db
+              :activated nil
+              :hovered nil
+              :entries new-entries))
+     )))
 
 ;;; subscriptions
 ;;; ------------------------------------------------------------------------------
 
 (register-sub :entries (fn [db _] (reaction (:entries @db))))
-(register-sub :over-idx (fn [db _] (reaction (:over-idx @db))))
-(register-sub :drag-idx (fn [db _] (reaction (:drag-idx @db))))
+(register-sub :window-width (fn [db _] (reaction (:window-width @db))))
+(register-sub :activated (fn [db _] (reaction (:activated @db))))
+(register-sub :hovered (fn [db _] (reaction (:hovered @db))))
 
 ;;; components 
 ;;; ------------------------------------------------------------------------------
 
-(defn sortable-list []
+(defn list-element []
+  (fn
+    [n entry]
+    (let [activated (subscribe [:activated])
+          hovered (subscribe [:hovered])]
+      [:li
+       {:data-idx n
+        :key n
+        :on-touch-start #(dispatch [:activate n (.-identifier (aget (.-targetTouches %) 0))])
+        :style {:background (cond (= n (:idx @activated)) "red"
+                                  (= (str n) @hovered) "blue"
+                                  :else "#f2f2f2")}
+        }
+      entry])))
+
+(defn sortable-list-component []
   (let [entries (subscribe [:entries])
-        over-idx (subscribe [:over-idx])
-        drag-idx (subscribe [:drag-idx])
-        ]
+        window-width (subscribe [:window-width])]
     [:ul
-     {:id "listparent"
-      :on-drag-over (fn [ev]
-                      (.preventDefault ev)
-                      (dispatch [:drag-over (.. ev -target -dataset -id)]))}
-     (doall (for [[n entry] (map-indexed (fn [idx entry] [idx entry]) @entries)]
-              (list
-               (if (= @over-idx n)
-                 [:li {:key "placeholder" :class "placeholder" :data-id "placeholder"}]
-                 nil)
-               [:li
-                {:draggable true
-                 :style {:display (if (= @drag-idx n) "none" "block")}
-                 :key n
-                 :data-id n
-                 :on-drag-start (fn [ev]
-                                  (set! (.. ev -dataTransfer -effectsAllowed) "move")
-                                  (dispatch [:drag-start n]))
-                 :on-drag-end (fn [ev]
-                                (.preventDefault ev)
-                                (set! (.. ev -dataTransfer -effectsAllowed) "move")
-                                (dispatch [:drag-end n]))
-                 }
-                entry]
-               )))
-     (list
-      (if (= @over-idx :end)
-        [:li {:key "placeholder" :class "placeholder" :data-id "placeholder"}]
-        nil)
-      [:li {:key "dummy-element" :class "dummy-element" :data-id "dummy-element"}])
-     ]
+     {:data-ww @window-width}
+     (for [[n entry](map-indexed vector @entries)]
+       ^{:key (str "entry" n)}
+       [list-element n entry])]
+    ))
+
+(defn update-dims
+  [this]
+  (let [parent-ul (reagent/dom-node this)
+        bounding-boxes 
+        (doall
+         (for [li (array-seq (.getElementsByTagName parent-ul "li"))]
+           (let [client-rect (.getBoundingClientRect li)
+                 top    (.-top client-rect)
+                 height (.-height client-rect)
+                 n (.. li -dataset -idx)
+                 ]
+             [top (+ top height) n]
+             )))]
+    (dispatch [:bounding-boxes bounding-boxes])
+    ))
+
+(def sortable-list 
+  (with-meta
+    (fn [] sortable-list-component)
+    {:component-did-mount update-dims
+     :component-did-update update-dims}
     ))
 
 (defn application []
@@ -126,10 +169,26 @@
 (defonce intialize
   (do
     (dispatch [:initialize])
+    (dispatch [:window-width (.-innerWidth js/window)])
     (.addEventListener js/window "keydown" 
                        #(case (.. % -keyCode)
                           :noop
-                          ))))
+                          ))
+    
+    (.addEventListener js/window "resize"
+                       (fn [ev] (dispatch [:window-width (.-innerWidth js/window)])))
+    
+    (.addEventListener js/window "touchmove"
+                       (fn [ev]
+                         (.preventDefault ev)
+                         (let [tch (aget (.-targetTouches ev) 0)]
+                           (dispatch [:touchmove [(.-pageX tch) (.-pageY tch)]]))))
+    
+    (.addEventListener js/window "touchend"
+                       (fn [ev]
+                         (let [tch (aget (.-changedTouches ev) 0)]
+                           (dispatch [:touchend (.-identifier tch)]))))
+    ))
 
 (defn ^:export run
   []
