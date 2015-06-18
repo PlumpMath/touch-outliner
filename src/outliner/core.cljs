10e6ff19730730edb1a78167acc12fcd52bfb069
minikomi <nerdfunk@gmail.com>
2015-06-18 14:16:44 +0900

    drag drop with mouse. not happy about :outside handling.

--- src/outliner/core.cljs
+++ src/outliner/core.cljs
@@ -1,7 +1,5 @@
 (ns ^:figwheel-always outliner.core
-    (:require-macros [reagent.ratom :refer [reaction]]
-                                        ; [secretary.core :refer [defroute]  ]
-                     )
+    (:require-macros [reagent.ratom :refer [reaction]])
     (:require
      [reagent.core :as reagent :refer [atom]]
      [re-frame.core :refer [register-handler
@@ -10,8 +8,7 @@
                             dispatch
                             dispatch-sync
                             subscribe]]
-     )
-    )
+     ))
 
 (enable-console-print!)
 
@@ -20,15 +17,31 @@
 ;;; state
 ;;; ------------------------------------------------------------------------------
 
+
 (def initial-state
   {:mode      :tree
    :state     :static
    :selected  nil 
-   :dragged   nil 
+   :dragged   nil
+   :hovered   nil
    :window-width 0
    :entries (vec (map #(apply str (take 5 (repeat (str % " ")))) (range 40))) 
    :bounding-boxes []
    })
+
+;;; util
+;;; ------------------------------------------------------------------------------
+
+(defn v-remove [v n]
+  (into (subvec v 0 n) (subvec v (inc n))))
+
+(defn move-within-vector [v from to]
+  (if (= from to) v
+      (let [elem      (v from)
+            without-v (v-remove v from) 
+            adj-to (if (< from to) (dec to) to)]
+        (into (conj (subvec without-v 0 adj-to) elem) (subvec without-v adj-to)))))
+
 
 ;;; handlers
 ;;; ------------------------------------------------------------------------------
@@ -43,7 +56,7 @@
  :item-mouse-down
  (fn 
    [db [_ idx]]
-   (assoc db :selected idx
+   (assoc db :selected (js/parseInt idx 10)
              :state    :pressed)
    ))
 
@@ -51,16 +64,39 @@
  :window-mouse-up
  (fn [db _]
    (cond
-     (= :pressed (:state db))
-     (do
-       (println "pressed " (:selected db))
-       (assoc db
-              :state :static
-              :selected nil
-              :dragged nil))
      
-     :else db
+     (= :pressed (:state db))
+     
+     (assoc db
+            :state :static
+            :hovered nil
+            :selected nil
+          )
+     
+     (= :dragging (:state db))
+     (do
+       (println (:hovered db))
+       (assoc db
+              :entries (if (= (:hovered db) :outside)
+                         (conj (v-remove (:entries db) (:selected db)) ((:entries db) (:selected db)))
+                         (move-within-vector (:entries db) (:selected db) (:hovered db)))
+             :state :static
+             :hovered nil
+             :selected nil
+             ))
+     
+     :else (assoc db
+                  :state :static
+                  :hovered nil
+                  :selected nil
+             )
      )))
+
+(register-handler                
+ :mounted-list
+ (fn 
+   [db [_ ul-dom-node]]
+   (assoc db :ul-dom-node ul-dom-node)))
 
 (register-handler                
  :window-width
@@ -80,12 +116,46 @@
 (register-handler                
  :window-mouse-move
  (fn 
-   [db _]
+   [db [_ page-x page-y]]
+   
    (cond
      
      (and (:selected db)
           (= :pressed (:state db)))
-     (assoc db :state :dragging)
+     
+     (assoc db
+            :state :dragging
+            :page-x page-x
+            :page-y page-y
+            )
+     
+     (and (:selected db)
+          (= :dragging (:state db)))
+     
+     (let [bounding-boxes
+           (doall
+            (for [li (array-seq (.getElementsByTagName (:ul-dom-node db) "li"))]
+              (let [top    (.-offsetTop li)
+                    height (.-offsetHeight li)
+                    n (.. li -dataset -idx)
+                    ]
+                [top height n]
+                )))
+           [_ _ hovered-box-idx] 
+           (or
+            (find-first
+             (fn [[y h _]]
+               (< (- page-y 10) (+ y h)))
+             bounding-boxes)
+            [nil nil :outside])
+           ]
+       (assoc db
+              :hovered (if (= :outside hovered-box-idx)
+                         :outside
+                         (js/parseInt hovered-box-idx 10))
+              :page-x page-x
+              :page-y page-y
+              ))
      
      :else db
      )))
@@ -93,8 +163,8 @@
 (register-handler
  :activate
  (fn [db [_ n id]]
-   (if (nil? (:activated db))
-     (assoc db :activated {:idx n :touchid id})
+   (if (nil? (:selected db))
+     (assoc db :selected {:idx n :touchid id})
      db)))
 
 ;;; subscriptions
@@ -102,7 +172,7 @@
 
 (register-sub :entries (fn [db _] (reaction (:entries @db))))
 (register-sub :window-width (fn [db _] (reaction (:window-width @db))))
-(register-sub :activated (fn [db _] (reaction (:activated @db))))
+(register-sub :selected (fn [db _] (reaction (:selected @db))))
 (register-sub :hovered (fn [db _] (reaction (:hovered @db))))
 
 ;;; components 
@@ -111,15 +181,18 @@
 (defn list-element []
   (fn
     [n entry]
-    (let [activated (subscribe [:activated])
+    (let [selected (subscribe [:selected])
           hovered   (subscribe [:hovered])]
       [:li
        {:data-idx n
         :key n
         :on-mouse-down #(dispatch [:item-mouse-down n ])
-        :style {:background (cond (= n (:idx @activated)) "red"
-                                  (= (str n) @hovered) "blue"
-                                  :else "#f2f2f2")}
+        :style {:border-top
+                (if
+                    (= n @hovered)
+                    "1px solid red"
+                    "1px solid transparent"
+                    )}
         }
       entry])))
 
@@ -133,28 +206,10 @@
        [list-element n entry])]
     ))
 
-(defn update-dims
-  [this]
-  (let [parent-ul (reagent/dom-node this)
-        bounding-boxes 
-        (doall
-         (for [li (array-seq (.getElementsByTagName parent-ul "li"))]
-           (let [client-rect (.getBoundingClientRect li)
-                 top    (.-top client-rect)
-                 height (.-height client-rect)
-                 n (.. li -dataset -idx)
-                 ]
-             [top (+ top height) n]
-             )))]
-    (dispatch [:bounding-boxes bounding-boxes])
-    ))
-
 (def sortable-list 
   (with-meta
     (fn [] sortable-list-component)
-    {;:component-did-mount update-dims
-     ;:component-did-update update-dims
-     }
+    {:component-did-mount (fn [this] (dispatch [:mounted-list (reagent/dom-node this)]))}
     ))
 
 (defn application []
@@ -179,7 +234,7 @@
     (.addEventListener js/window "mousemove"
                        (fn [ev]
                          (.preventDefault ev)
-                         (dispatch [:window-mouse-move])
+                         (dispatch [:window-mouse-move (.. ev -pageX) (.. ev -pageY)])
                          ))
     
     (.addEventListener js/window "mouseup"
